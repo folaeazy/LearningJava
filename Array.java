c476ee79805375cfb181c17ad2cd3c260796f68a
folaeazy <folaisrael11@gmail.com>
2025-01-08 15:17:39 +0100

    worked on diff type of array and object

--- Array.java
+++ Array.java
@@ -44,15 +44,76 @@ public class Array
         return 0;
     }
 
+
     public static void main(String[] args)
     {
         Array objs = new Array();
 
         //objs.singleArray();
-        objs.multiArray();
+        //objs.multiArray();
+        Student s1 = new Student(); // creating object of the class for each studnet 
+        s1.rollNo = 1;
+        s1.name = "John";
+        s1.score = (byte) 10;
+
+
+        Student s2 = new Student();
+        s2.rollNo = 2;
+        s2.name = "Ratmond";
+        s2.score = (byte) 50;
+
+
+        Student s3 = new Student();
+        s3.rollNo = 3;
+        s3.name = "Paul";
+        s3.score = (byte) 40;
+      
+
+      //creating an array of the student objects
+
+      Student students[] = new Student[3];
+      students[0] = s1;
+      students[1] = s2;
+      students[2] = s3;
+
+      s1.objectArray(students.length,students); // for normal for loop syntax
+      s1.objectArray(students); // for enhanced for loop .
+    }
+   
+
+}
+
+// "object array" --class for student.
+class Student 
+{
+    public int rollNo;
+    public String name;
+    public byte score;
+
+    public void objectArray(int array, Student students[])
+    {
+       
+        for (int i = 0; i < array ; i++)
+        { 
+            System.out.println("roll no : " + students[i].rollNo + " name : " + students[i].name + " score: " + students[i].score);
+        }
+
+
+       
     
     }
 
+     //using enhanced loop or forEach loop syntax and method overloading
+    public void objectArray(Student students[])
+    {
+        
+        for (Student student : students)
+        {
+            System.out.println("roll no : " + student.rollNo + " name : " + student.name + " the score is : " + student.score);
+        }
+
+    }
+    
 }
 
 
---
+++ StringsDemo.java
@@ -0,0 +1,17 @@
+//strings in java .......
+
+public class StringsDemo
+{
+
+     public static void main(String[] args)
+     {
+
+        // string object 
+        String name =  new String("Raymond");
+         //or 
+        String names = "Raymond";
+
+        //String buffer
+        StringBuffer names = "my name";
+     }
+}
