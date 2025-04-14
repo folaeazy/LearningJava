711dd12d20c07f9c0af27952f0ae27f2af16e8e0
folaeazy <folaeazy0423@gmail.com>
2025-04-14 01:29:29 +0100

    added array package:git

---
+++ array/Array.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Array {
+
+
+
+
+    public static void main(String[] args) {
+
+        // array syntax 
+        // data_type[]  variable_name = new data_type[size]  @@object creating happen at runtime in the heap memory
+
+    }
+
+}
